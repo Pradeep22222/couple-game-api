1f87045f1cface91abb3fb43a59e737861ddc0d8
PK <pradeepdhital124@gmail.com>
2023-01-13 10:51:54 +1100

    post and get handled in numbers

--- server.js
+++ server.js
@@ -2,8 +2,22 @@ import express from "express";
 import numberRouter from "./src/routers/numberRouter.js";
 const app = express();
 const PORT = 8000;
+// middlewares
+app.use(express.json());
 app.use("/api/v1/game", numberRouter);
-
+app.use("/", (req, res) => {
+  res.json({ 
+    status: "success",
+    message:"you reached to the couple game api"
+  })
+})
+app.use((error, req, res, next) => {
+  const status = error.status || 404;
+  res.status(status).json({
+    status: "error",
+    message: error.message
+  })
+});
 app.listen(PORT, (err) => {
   err && console.log(err);
   console.log(`server is running on the port ${PORT}`);
--- src/routers/numberRouter.js
+++ src/routers/numberRouter.js
@@ -1,26 +1,35 @@
 import express from "express";
 const router = express.Router();
-
-router.post("/number", (req, res) => {
-  try {
-     res.json({
-       status: "success",
-       message: "number return from post method",
-     });
-  } catch (error) {
-    
+const fakeNumbers = [
+  {
+    "urNumber": 937549234,
+    "partnerNumber":853257
   }
- 
+]
+router.post("/number", (req, res, next) => {
+  try {
+    fakeNumbers.push(req.body);
+    console.log(fakeNumbers)
+    res.json({
+      status: "success",
+      message: "number return from post method",
+    });
+  } catch (error) {
+    error.status = 404;
+    next(error);
+  }
 });
-router.get("/number", (req, res) => {
+router.get("/number", (req, res, next) => {
   try {
-      res.json({
-        status: "success",
-        message: "number return from get method",
-      });
+    console.log(req.body);
+    res.json({
+      status: "success",
+      message: "number return from get method",
+      fakeNumbers,
+    });
   } catch (error) {
-    
+    error.status = 500;
+    next(error);
   }
-
 });
 export default router;
